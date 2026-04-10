3a3cee2c646f630bb848cda9d0e568c411366cb0
ponder-j <a2140316092@qq.com>
2026-04-10 17:00:05 +0800

    Auto update at 2026/04/10 17:00:05

--- Lab1/codes/main.c
+++ Lab1/codes/main.c
@@ -1,30 +1,30 @@
-#include <stdio.h>
 #include "tree.h"
+#include <stdio.h>
 
-extern int yyrestart(FILE* f);
+extern int yyrestart(FILE *f);
 extern int yyparse();
 
-int error = 0;
-Node* Root = NULL;
+int error = 0;     // 错误计数器
+Node *Root = NULL; // 语法树根节点
 
-int main(int argc, char** argv) 
-{
-    if (argc <= 1) return 1;
-    FILE* f = fopen(argv[1], "r");
-    if (!f) {
-        perror(argv[1]);
-        return 1;
-    }
-    
-    yyrestart(f);
-    yyparse();
-    
-    // 如果词法和语法分析过程中没有发现错误，才打印语法树
-    if (error == 0 && Root != NULL) {
-        printTree(Root, 0);
-    }
-    
-    delNode(Root); // 释放语法树内存空间
-    fclose(f);
-    return 0;
+int main(int argc, char **argv) {
+  if (argc <= 1)
+    return 1;
+  FILE *f = fopen(argv[1], "r");
+  if (!f) {
+    perror(argv[1]);
+    return 1;
+  }
+
+  yyrestart(f);
+  yyparse();
+
+  // 如果词法和语法分析过程中没有发现错误，才打印语法树
+  if (error == 0 && Root != NULL) {
+    printTree(Root, 0);
+  }
+
+  delNode(Root); // 释放语法树内存空间
+  fclose(f);
+  return 0;
 }
